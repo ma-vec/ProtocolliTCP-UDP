d032dede196503056695caa2fddeae7d36eaa25a
ma-vec <maurov2006@gmail.com>
2025-05-09 13:47:18 +0200

    TCP: Somma tra numeri nel client

---
+++ TCP/TCP/Program.cs
@@ -0,0 +1,68 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Socket Listener acts as a server and listens to the incoming
+// messages on the specified port and protocol.
+public class SocketListener
+{
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+
+    public static void StartServer()
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+
+        try
+        {
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            Console.WriteLine("Waiting for a connection...");
+
+            while (true)
+            {
+                // Aspetta una connessione in modo continuo
+                Socket handler = listener.Accept();
+                string data = null;
+                byte[] bytes = new byte[1024];
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data != null)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received: {0}", data);
+
+                string[] arrayChars = data.Split(",");
+                int somma = Convert.ToInt32(arrayChars[0]) + Convert.ToInt32(arrayChars[1]);
+                data = somma.ToString();
+
+                byte[] msg = Encoding.ASCII.GetBytes(data);
+                handler.Send(msg);
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress any key to continue...");
+        Console.ReadKey();
+    }
+
+}
