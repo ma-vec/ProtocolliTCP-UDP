24725514b88136a0636b7a34f3d7f91a2fa4db37
ma-vec <maurov2006@gmail.com>
2025-05-11 14:04:16 +0200

    UDP: con app.config e ConfigurationManager

---
+++ UDP_protocol/Client/Program.cs
@@ -0,0 +1,36 @@
+﻿// Client-Side Implementation Of UDP: 
+using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+class StudentUDPClient
+{
+    public static void Main(string[] args)
+    {
+        UdpClient udpc = new UdpClient("127.0.0.1", 7878);
+        IPEndPoint ep = null;
+        while (true)
+        {
+            Console.Write("Enter Your Name: ");
+            string studentName = Console.ReadLine();
+
+
+            // Check weather student entered name to start conversation 
+            if (studentName == "")
+            {
+                Console.Write("You did not enter your name. Closing...");
+                break;
+            }
+            // Data to send 
+            byte[] msg = Encoding.ASCII.GetBytes(studentName);
+            udpc.Send(msg, msg.Length);
+
+            // received Data 
+            byte[] rdata = udpc.Receive(ref ep);
+            string job = Encoding.ASCII.GetString(rdata);
+            Console.WriteLine(job);
+        }
+    }
+}
+
---
+++ UDP_protocol/UDP_protocol/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Configuration;
+
+class StudentUDPServer
+{
+    public static void Main()
+    {
+        UdpClient udpc = new UdpClient(7878);
+        Console.WriteLine("Server Started, servicing on port no. 7878");
+        IPEndPoint ep = null;
+
+        while (true)
+        {
+            byte[] receivedData = udpc.Receive(ref ep);
+            string studentName = Encoding.ASCII.GetString(receivedData);
+
+            string msg = ConfigurationManager.AppSettings[studentName];
+            if (msg == null) msg = "No such Student available for conversation";
+            byte[] sdata = Encoding.ASCII.GetBytes(msg);
+            udpc.Send(sdata, sdata.Length, ep);
+        }
+    }
+}
