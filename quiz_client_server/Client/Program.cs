9c07ba1083d019249f0e618de49601dde7623dd5
ma-vec <maurov2006@gmail.com>
2025-05-16 13:59:40 +0200

    Quiz: V01 not work

---
+++ quiz_client_server/Client/Program.cs
@@ -0,0 +1,81 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+using System.Threading;
+
+namespace quiz_client_server
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            StartClient();
+        }
+
+        public static void StartClient()
+        {
+            byte[] bytes = new byte[1024];
+
+            try
+            {
+                // Ottieni l'indirizzo IP del server
+                IPHostEntry host = Dns.GetHostEntry("localhost");
+                IPAddress ipAddress = host.AddressList[0];
+                IPEndPoint remoteEP = new IPEndPoint(ipAddress, 11000);
+
+                // Crea un socket TCP/IP
+                Socket sender = new Socket(ipAddress.AddressFamily,
+                    SocketType.Stream, ProtocolType.Tcp);
+
+                // Connetti il socket all'endpoint remoto
+                sender.Connect(remoteEP);
+                Console.WriteLine("Socket connesso a {0}", sender.RemoteEndPoint.ToString());
+
+                // Messaggio iniziale da inviare
+                string strToSend = "ciao";
+
+                // Codifica la stringa in un array di byte
+                byte[] msg = Encoding.ASCII.GetBytes(strToSend);
+                int bytesSent = sender.Send(msg);
+                Console.WriteLine("Messaggio inviato: {0}", strToSend);
+
+                // Ricevi la risposta dal server
+                int bytesRec = sender.Receive(bytes);
+                Console.WriteLine("Risposta ricevuta: {0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+
+                // Richiedi input all'utente
+                Console.WriteLine("Inserisci la tua scelta:");
+                /*Console.WriteLine("{0}", Encoding.ASCII.GetString(bytes, 0, bytesRec));
+                int S1 = sender.Receive(bytes);
+                Console.WriteLine("{0}", Encoding.ASCII.GetString(bytes, 0, S1));
+                int S2 = sender.Receive(bytes);
+                Console.WriteLine("{0}", Encoding.ASCII.GetString(bytes, 0, S2));
+                int S3 = sender.Receive(bytes);
+                Console.WriteLine("{0}", Encoding.ASCII.GetString(bytes, 0, S3));*/
+                string scelta = "1,1";
+
+                // Invia la scelta al server
+                byte[] ris = Encoding.ASCII.GetBytes(scelta);
+                int bytesSent1 = sender.Send(ris);
+                Console.WriteLine("Scelta inviata: {0}", scelta);
+
+                // Rilascia il socket
+                sender.Shutdown(SocketShutdown.Both);
+                sender.Close();
+            }
+            catch (ArgumentNullException ane)
+            {
+                Console.WriteLine("ArgumentNullException : {0}", ane.ToString());
+            }
+            catch (SocketException se)
+            {
+                Console.WriteLine("SocketException : {0}", se.ToString());
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine("Unexpected exception : {0}", e.ToString());
+            }
+        }
+    }
+}
---
+++ quiz_client_server/quiz_client_server/Program.cs
@@ -0,0 +1,95 @@
+﻿using System;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+// Socket Listener acts as a server and listens to the incoming
+// messages on the specified port and protocol.
+public class SocketListener
+{
+    public static int Main(String[] args)
+    {
+        StartServer();
+        return 0;
+    }
+
+    public static void StartServer()
+    {
+        IPHostEntry host = Dns.GetHostEntry("localhost");
+        IPAddress ipAddress = host.AddressList[0];
+        IPEndPoint localEndPoint = new IPEndPoint(ipAddress, 11000);
+        string result="4";
+
+        try
+        {
+            Socket listener = new Socket(ipAddress.AddressFamily, SocketType.Stream, ProtocolType.Tcp);
+            listener.Bind(localEndPoint);
+            listener.Listen(10);
+            Console.WriteLine("Waiting for a connection...");
+
+            while (true)
+            {
+                // Aspetta una connessione in modo continuo
+                Socket handler = listener.Accept();
+                string data = null;
+                byte[] bytes = new byte[1024];
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data != null)
+                    {
+                        break;
+                    }
+                }
+
+                Console.WriteLine("Text received: {0}", data);
+                //DOMANDA 1
+                byte[] q1 = Encoding.ASCII.GetBytes("Quale e' la capitale della Norvegia?\n");
+                handler.Send(q1);
+                byte[] qd1 = Encoding.ASCII.GetBytes("1) Oslo\n");
+                handler.Send(qd1);
+                byte[] qd2 = Encoding.ASCII.GetBytes("2) Botttanuco\n");
+                handler.Send(qd2);
+                byte[] qd3 = Encoding.ASCII.GetBytes("3) Otranto\n");
+                handler.Send(qd3);
+              
+
+                while (true)
+                {
+                    bytes = new byte[1024];
+                    int bytesRec = handler.Receive(bytes);
+                    data += Encoding.ASCII.GetString(bytes, 0, bytesRec);
+                    if (data != null)
+                    {
+                        break;
+                    }
+
+                }
+                string[] arrayChars = data.Split(",");
+                if (arrayChars[0]=="1")
+                {
+                    if (arrayChars[1] == "1")
+                        result = "BRAVO BOMBERONE";
+                    else result = "PAGLIACCIOOOO";
+                }
+
+                byte[] risultato = Encoding.ASCII.GetBytes(result);
+                handler.Send(risultato);
+
+                handler.Shutdown(SocketShutdown.Both);
+                handler.Close();
+            }
+        }
+        catch (Exception e)
+        {
+            Console.WriteLine(e.ToString());
+        }
+
+        Console.WriteLine("\nPress any key to continue...");
+        Console.ReadKey();
+    }
+
+}
